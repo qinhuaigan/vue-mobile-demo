82248b2dd651ffff967a2504d29c4d02a4c977b5
qinhuaigan <624518812@qq.com>
2020-09-24 16:03:27 +0800

    修改 store 中的方法，为了使用方便

--- store/index.js
+++ store/index.js
@@ -6,20 +6,24 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
     userInfo: {}, // 登录用户信息
-    messageNum: 0
+    num: 0
   },
   mutations: {
     // 更新 state 中的值, 当 state 中的值更新时, 让所有用到 state 的地方, 都刷新视图
-    /*
-        this.$store.commit('increment', {
-          name: '', // 要更新的 state 中的字段名称
-          value: val // 要更新的 state 中的字段的值
-        })
-    */
     increment (state, obj) {
-      state[obj.name] = obj.value
+      state[obj.label] = obj.value
     }
   }
 })
+
+// 拓展 Vue setStoreData() 函数,来修改 store 中的 state 对应的值
+// 使用方法: this.setStoreData(label, value)
+// 说明: label 对应的是 store.state 的属性名称, value 对应的是 store.state 的属性值
+Vue.prototype.setStoreData = function (label, value) {
+  store.commit('increment', {
+    label,
+    value
+  })
+}
 
 export default store
